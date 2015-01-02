ef87be0b2ecd873987200173c1286a478d6d5f20
Claudia Luque Fernández <claudia.luque.fernandez@gmail.com>
2015-01-02 15:35:51 +0100

    Playing with messages between handheld -> wearable.

--- RecargaTussam/src/recargaAucorsa/java/es/claucookie/recarga/MainActivity.java
+++ RecargaTussam/src/recargaAucorsa/java/es/claucookie/recarga/MainActivity.java
@@ -23,6 +23,12 @@ import com.android.volley.RequestQueue;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.Volley;
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 import com.mobivery.android.helpers.TagFormat;
 import com.mobivery.android.widgets.ExLabel;
 import com.mobivery.android.widgets.ExText;
@@ -50,7 +56,9 @@ import es.claucookie.recarga.model.dto.TussamCardDTO;
 import es.claucookie.recarga.model.dto.TussamCardsDTO;
 
 @EActivity(R.layout.activity_main2)
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener  {
     public static final String STATUS_URL = "http://recargas.aucorsa.es/checkcard.php?nCard=";
     public static final String CREDIT_URL = "http://recargas.aucorsa.es/checkcard.php?nCard=";
     public static final long ONE_MINUTE = 60 * 1000; // Millisecs
@@ -127,6 +135,7 @@ public class MainActivity extends ActionBarActivity {
     boolean isDetailView = false, isAddView = false, isEditView = false;
 
     private ArrayAdapter<String> spinnerAdapter;
+    GoogleApiClient googleClient;
 
     /**
      * Global request queue for Volley
@@ -146,6 +155,20 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         Crashlytics.start(this);
 
+        // Build a new GoogleApiClient
+        googleClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+    // Connect to the data layer when the Activity starts
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleClient.connect();
     }
 
     @Override
@@ -599,4 +622,57 @@ public class MainActivity extends ActionBarActivity {
         return text;
     }
 
+    /** Google API methods
+     *
+     *
+     */
+
+    // Send a message when the data layer connection is successful.
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        String message = "Hello wearable\n Via the data layer";
+        //Requires a new thread to avoid blocking the UI
+        new SendToDataLayerThread("/message_path", message).start();
+    }
+
+    // Disconnect from the data layer when the Activity stops
+    @Override
+    protected void onStop() {
+        if (null != googleClient && googleClient.isConnected()) {
+            googleClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    // Placeholders for required connection callbacks
+    @Override
+    public void onConnectionSuspended(int cause) { }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) { }
+
+    class SendToDataLayerThread extends Thread {
+        String path;
+        String message;
+
+        // Constructor to send a message to the data layer
+        SendToDataLayerThread(String p, String msg) {
+            path = p;
+            message = msg;
+        }
+
+        public void run() {
+            NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes(googleClient).await();
+            for (Node node : nodes.getNodes()) {
+                MessageApi.SendMessageResult result = Wearable.MessageApi.sendMessage(googleClient, node.getId(), path, message.getBytes()).await();
+                if (result.getStatus().isSuccess()) {
+                    Log.v("myTag", "Message: {" + message + "} sent to: " + node.getDisplayName());
+                }
+                else {
+                    // Log an error
+                    Log.v("myTag", "ERROR: failed to send Message");
+                }
+            }
+        }
+    }
 }
